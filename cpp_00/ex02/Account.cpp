e50d24bea1bb53f2589102cda1f4f81c9e5bf9b3
l <maelapouge@gmail.com>
2025-08-18 18:32:03 +0200

    Updated (default commit)
    	new file:   19920104_091532.log
    	new file:   Account.cpp
    	new file:   Account.hpp
    	new file:   tests.cpp
    	new file:   ../../cpp_01/ex00/Makefile
    	new file:   ../../cpp_01/ex00/Zombie.cpp
    	new file:   ../../cpp_01/ex00/Zombie.hpp
    	new file:   ../../cpp_01/ex00/main.cpp
    	new file:   ../../cpp_01/ex00/newZombie.cpp
    	new file:   ../../cpp_01/ex00/randomChump.cpp

---
+++ cpp_00/ex02/Account.cpp
@@ -0,0 +1,58 @@
+#include "Account.hpp"
+#include <iostream>
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+void	Account::Account(int initialDeposit)
+{
+	this->index = _nbAccount;
+	_nbAccount++;
+	this->_amount = initialDeposit;
+	_totalAmount += initialDeposit;
+	this->_accountIndex = initialDeposit;
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";created" << std::endl;
+}
+
+void	Account::~Account(void)
+{
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";closed" << std::endl;
+}
+
+void	Account::displayAccountsInfos(void)
+{
+	std::cout << "accounts:" << this->getNbAccounts << ";total:" << this->_getTotalAmount << ";deposits" << this->_getNbDeposits << ";withdrawals" << this->_getNbWithdrawals << std::endl;
+}
+
+
+void	Account::displayStatus (void)
+{
+	std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount << ";deposit:" << "initial - total?" << ";amount:" << this->_amount << ";nb_deposits" << this->_nbDeposits << std::endl;
+}
+
+
+
+// deposits et withdrawals negatifs ?
+
+
+
+void	Account::makeDeposit(int deposit)
+{
+	this->nb_deposits++;
+	this->_amount += deposit;
+	_totalAmount += deposit;
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	this->nb_withdrawals++;
+	this->_amount -= withdrawal;
+	_totalAmount -= withdrawal;
+}
+
+int		Account::checkAmount(void)
+{
+
+}
