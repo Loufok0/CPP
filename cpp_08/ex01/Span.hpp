9e577dd2500947c206041d10001c2533f710dce0
l <maelapouge@gmail.com>
2026-01-14 07:39:46 +0100

    init of ex01

---
+++ cpp_08/ex01/Span.hpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+class Span
+{
+	private:
+		unsigned int	N;
+		std::span<T> 		S
+	public:
+		Span(unsigned in N);
+		Span(const Span& other);
+		Span& operator = (const Span& other);
+		~Span();
+
+
+		addNumber();
+		shortestSpan();
+		longestSpan();
+}
