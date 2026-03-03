58db530648f05b039d024cb9c458de2bb0781aa1
l <maelapouge@gmail.com>
2026-03-03 17:55:15 +0100

    added more main verif to the soup...

--- cpp_08/ex02/main.cpp
+++ cpp_08/ex02/main.cpp
@@ -2,6 +2,81 @@
 
 int main()
 {
+    std::cout << "=== Basic stack test ===" << std::endl;
+
+    MutantStack<int> mstack;
+
+    mstack.push(5);
+    mstack.push(17);
+
+    std::cout << "Top: " << mstack.top() << std::endl;
+
+    mstack.pop();
+
+    std::cout << "Size after pop: " << mstack.size() << std::endl;
+
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
+
+    std::cout << "\n=== Iteration test ===" << std::endl;
+
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+
+    std::cout << "\n=== Const iteration test ===" << std::endl;
+
+    const MutantStack<int> constStack(mstack);
+    MutantStack<int>::const_iterator cit = constStack.begin();
+    MutantStack<int>::const_iterator cite = constStack.end();
+
+    while (cit != cite)
+    {
+        std::cout << *cit << std::endl;
+        ++cit;
+    }
+
+    std::cout << "\n=== Comparison with std::stack ===" << std::endl;
+
+    std::stack<int> s(mstack);
+    std::cout << "std::stack size: " << s.size() << std::endl;
+
+    std::cout << "\n=== String test ===" << std::endl;
+
+    MutantStack<std::string> stringStack;
+    stringStack.push("Hello");
+    stringStack.push("Mutant");
+    stringStack.push("Stack");
+
+    for (MutantStack<std::string>::iterator sit = stringStack.begin();
+         sit != stringStack.end();
+         ++sit)
+    {
+        std::cout << *sit << std::endl;
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+/*
+int main()
+{
 	{
 		MutantStack<int> mstack;
 		mstack.push(5);
@@ -53,3 +128,5 @@ int main()
 	return 0;
 
 }
+
+*/
